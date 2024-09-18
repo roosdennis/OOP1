b35649fdd94e7c0c56d981f205731c34a5cc0c17
roosdennis <dennisroos87@gmail.com>
2024-09-18 19:16:55 +0200

    Start Homework 240918

---
+++ Week_2/src/Calculator.java
@@ -0,0 +1,42 @@
+public class Calculator {
+
+    public static int getMenuOption(){
+        return 0;
+    }
+
+    public static double getOperand(String prompt){
+        return 0;
+    }
+
+    public static double[] getPair(){
+        return 0;
+    }
+
+    public static double add(double[] values){
+        return 0;
+    }
+
+    public static double subtract(double[] values){
+        return 0;
+    }
+
+    public static double multiply(double[] values){
+        return 0;
+    }
+
+    public static double divide(double[] values){
+        return 0;
+    }
+
+    public static double random(double[] values){
+        return 0;
+    }
+
+    public static double distance(double[] start, double[] stop){
+        return 0;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
--- Week_2/src/Fermat.java
+++ Week_2/src/Fermat.java
@@ -2,8 +2,14 @@ public class Fermat {
     public static int fermat(int a, int b, int c, int n){
         if ((Math.pow(a,n) + Math.pow(b,n)) == Math.pow(c,n)){
             System.out.println("Potverdorie, Fermat had geen gelijk!");
+            return 0;
         } else{
             System.out.println("Nee, dat werkt niet");
+            return 1;
         }
+    }
+
+    public static void main(String[] args) {
+        fermat(3,4,5,3);
     }
 }
