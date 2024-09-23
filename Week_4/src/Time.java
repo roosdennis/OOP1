fb53dc92516975c7dcc7446782844df70c935939
Dennis Roos <dennisroos87@gmail.com>
2024-09-23 19:14:47 +0200

    test2

--- Week_4/src/Time.java
+++ Week_4/src/Time.java
@@ -1,2 +1,31 @@
 public class Time {
+
+    //Fields (variabelen)
+    // <access-modifier> <datatype> <naam> = <initialisatie>
+
+    private int minute;
+    private int second;
+
+    //Constructors
+    public Time() {
+        System.out.println("Dit is de constructor");
+    }
+    //Methods
+    public int getMinute() {
+        return minute;
+    }
+
+    public int getSecond() {
+        return second;
+    }
+
+    public void setMinute(int minute){
+        this.minute = minute;
+    }
+
+    public void setSecond(int second) {
+        this.second = second;
+    }
+
+
 }
